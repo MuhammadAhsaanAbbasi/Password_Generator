abf86ef7de146b8d1da54183fbe267e7db5899db
MuhammadAhsaanAbbasi <mahsaanabbasi@gmail.com>
2023-12-12 21:19:24 +0500

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,170 @@
+'use client'
 import Image from 'next/image'
+import { motion, AnimatePresence, useAnimation } from "framer-motion"
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useInView } from "react-intersection-observer";
+
+const users = ["Hamzah", "Ali", "Okasha",]
+const stds = ["Hamzah", "Ali", "Okasha",]
+
+const menu = [
+  {
+    title: "Biryani",
+    description: 'Lazeez Biryani',
+    price: 500,
+    coldDrink: true,
+    image: '/images/biryani.jpg'
+  },
+  {
+    title: "Tikka",
+    description: 'Lazeez Tikka',
+    price: 600,
+    coldDrink: true,
+    image: '/images/tikka.jpg'
+  },
+  {
+    title: "Shawarma",
+    description: 'Lazeez Shawarma',
+    price: 200,
+    coldDrink: true,
+    image: '/images/shawarma.jpg'
+  },
+  {
+    title: "Karhai",
+    description: 'Lazeez Karhai',
+    price: 800,
+    coldDrink: true,
+    image: '/images/karhai.jpeg'
+  },
+  {
+    title: "Fish & Grill",
+    description: 'Lazeez Fish & Grill',
+    price: 900,
+    coldDrink: true,
+    image: '/images/fish.jpg'
+  },
+]
 
 export default function Home() {
+  const [show, setShow] = useState(false)
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  const fadeInOut = {
+    hidden: { opacity:0, y:-50  },
+    visible: { opacity: 1, y:50 },
+  };
+
+  // useEffect(() => {
+  //   if (inView) {
+  //     controls.start("visible");
+  //   } else {
+  //     controls.start("hidden");
+  //   }
+  // }, [controls, inView]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className='h-screen'>
+
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className='flex flex-col'>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          transition={{
+            layout: {
+              duration: 1,
+              type: 'spring',
+            }
+          }}
+          layout
+          onClick={() => setShow(!show)} className='p-8 bg-orange-300 w-[50vw]'>
+          {show ? "Not Showing" : "Showing"}
+          {show && (
+            <motion.p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, minus alias. Optio id eveniet culpa doloribus commodi eligendi eum, eius ipsum numquam enim, aspernatur perspiciatis est repellendus excepturi voluptatem eaque!
+            </motion.p>
+          )}
+          <div className='w-[90%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10'>
+            <AnimatePresence>
+              {show &&
+                menu.map((item, id) => {
+                  return (
+                    <motion.div
+                      ref={ref}
+                      initial="hidden"
+                      animate={controls}
+                      variants={fadeInOut}
+                      exit={{opacity:0, y:-50}}
+                      transition={{ duration: 1.5 }}
+                      onViewportEnter={() => controls.start("visible")}
+                      onViewportLeave={() => controls.start("hidden")}
+                      key={id} className='border bg-yellow-400 m-4 rounded-md p-5 shadow-2xl'>
+                      <Image src={item.image} alt='ronaldo' width={300} height={300} className="w-[300px] object-cover" />
+                      <div className='mt-5'>
+                        <p>{item.title}</p>
+                        <p>{item.description}</p>
+                        <p>{item.price}</p>
+                        <p>{item.coldDrink ? "Pepsi" : "Water"}</p>
+                      </div>
+                    </motion.div>
+                  )
+                })
+              }
+            </AnimatePresence>
+          </div>
+        </motion.button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className='h-screen'>
+        {/* <motion.div
+        className='my-6 m-auto h-40 w-40 bg-blue-900'
+        initial={{x:0, y:0, scale:0.4}}
+        animate={{rotate:360, scale:1.5}}
+        exit={{rotate:-360, scale:-0.5}}
+        transition={{
+          duration:3,
+          repeat: Infinity,
+        }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </motion.div> */}
       </div>
-    </main>
+    </>
   )
 }
+      {/* <div className='grid grid-cols-12 rounded-full h-screen'>
+      <div className='bg-red-800 p-10 border border-black col-span-3 text-gray-400 text-lg font-bold'>
+        Sidebar
+        <ul>
+        <li>_________________________</li>
+        <li>_________________________</li>
+        <li>_________________________</li>
+        <li>_________________________</li>
+        <li>_________________________</li>
+        </ul>
+      </div>
+      <div className='bg-gray-700 p-10 border border-black col-span-9 text-lg font-bold'>
+        Main Content
+      <ul className='text-gray-400'>
+        <li>_________________________</li>
+        <li>_________________________</li>
+        <li>_________________________</li>
+        <li>_________________________</li>
+        <li>_________________________</li>
+        </ul>
+      </div>
+    </div> */}
+      {/* <div className='grid grid-cols-12 grid-rows-5 gap-2'>
+      <div className='col-span-12 h-20 bg-gray-200 row-span-1'>
+          Navbar
+      </div>
+      <div className='row-span-3 col-span-3 bg-gray-300'>
+        Sidebar
+      </div>
+      <div className='row-span-3 col-span-9 bg-gray-400'>
+          Hero Component 
+      </div>
+      <div className='col-span-12 h-20 bg-gray-500 row-span-1'>
+          Footer
+      </div>
+    </div> */}
